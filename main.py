b67903a4601729dec297cfc8a4f36574c4677703
Martin Miloslav Suquillo Tigasi <116769944+31MartinS@users.noreply.github.com>
2024-06-07 07:52:27 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World my name is Martín"}
-    return jsonify(list(my_set))
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+    
